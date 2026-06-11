fe3c626192f15a2d24ef35bf44a5c45eca7dd6f1
Anna Piekarska <13958030+annapiekarska@users.noreply.github.com>
2026-06-11 21:35:37 +0200

    Use API base URL from environment

--- test-data/apiUrls.ts
+++ test-data/apiUrls.ts
@@ -3,7 +3,8 @@ const apiBaseUrl = process.env.API_BASE_URL;
 if (!apiBaseUrl) {
   throw new Error('API_BASE_URL is not configured');
 }
+
 export const apiUrls = {
-  users: 'https://jsonplaceholder.typicode.com/users',
-  posts: 'https://jsonplaceholder.typicode.com/posts',
+  users: `${apiBaseUrl}/users`,
+  posts: `${apiBaseUrl}/posts`,
 };
